1eb02a822a399a123c9302031d7062e223630a0f
Bum Kim <bum.s.kim42@gmail.com>
2021-06-03 13:17:13 -0400

    labeled birthday distribution

--- stats/static/stats/main.js
+++ stats/static/stats/main.js
@@ -288,7 +288,7 @@ var ageRange = new Chart(ctx, {
     data: {
         labels: Object.keys(dobs),
         datasets: [{
-            label: "remove me",
+            label: "day, month, birthdays",
             data: points,
             backgroundColor: 'rgba(54, 162, 235, 0.2)',
             borderColor: 'rgba(54, 162, 235, 1)',
